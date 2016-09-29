8c8289d1a8482fc3d3b3ef3a7ccfdcc67bdf2f69
Lindsay Garces <Lindsay Garces>
2016-09-28 22:18:42 -0400

    corrected files

--- final_assignment.py
+++ final_assignment.py
@@ -23,42 +23,42 @@ if __name__ == "__main__":
 
 
 #YELP_API
-from yelp.client import Client
-from yelp.oauth1_authenticator import Oauth1Authenticator
-import os
+#from yelp.client import Client
+#from yelp.oauth1_authenticator import Oauth1Authenticator
+#import os
 
-from dotenv import load_dotenv, find_dotenv
-load_dotenv(find_dotenv())
+#from dotenv import load_dotenv, find_dotenv
+#load_dotenv(find_dotenv())
 
-def get_businesses(location, term):
-	auth = Oauth1Authenticator(
-		consumer_key=os.environ['CONSUMER_KEY'],
-		consumer_secret=os.environ['CONSUMER_SECRET'],
-		token=os.environ['TOKEN'],
-		token_secret=os.environ['TOKEN_SECRET']
-	)
+#def get_businesses(location, term):
+#	auth = Oauth1Authenticator(
+#		consumer_key=os.environ['CONSUMER_KEY'],
+#		consumer_secret=os.environ['CONSUMER_SECRET'],
+#		token=os.environ['TOKEN'],
+#		token_secret=os.environ['TOKEN_SECRET']
+#	)
 
-	client = Client(auth)
+#	client = Client(auth)
 
-	params = {
-		'term': term,
-		'lang': 'en',
-		'limit': 3
-	}
+#	params = {
+#		'term': term,
+#		'lang': 'en',
+#		'limit': 3
+#	}
 
-	response = client.search(location, **params)
+#	response = client.search(location, **params)
 
-	businesses = []
+#	businesses = []
 
-	for business in response.businesses:
+#	for business in response.businesses:
 		#print(business.name, business.rating, business.phone)
-		businesses.append({"name": business.name,
-			"rating": business.rating,
-			"phone": business.phone
-		})
+#		businesses.append({"name": business.name,
+#			"rating": business.rating,
+#			"phone": business.phone
+#		})
 
-	return businesses
+#	return businesses
 
-businesses = get_businesses('New York City', 'cocktails')
+#businesses = get_businesses('New York City', 'cocktails')
 
-print(businesses)
+#print(businesses)
--- web.py
+++ web.py
@@ -21,20 +21,20 @@ if __name__ == "__main__":
 
 
 #WEATHER
-import forecastio
-from geopy.geocoders import Nominatim
+#import forecastio
+#from geopy.geocoders import Nominatim
 
-import os
-from dotenv import load_dotenv, find_dotenv
-load_dotenv(find_dotenv())
+#import os
+#from dotenv import load_dotenv, find_dotenv
+#load_dotenv(find_dotenv())
 
-def get_weather(address):
-	api_key = os.environ['FORECASTIO_API_KEY']
-	geolocator = Nominatim()
-	location = geolocator.geocode(address)
-	latitude = location.latitude
-	longitude = location.longitude
-	forecast = forecastio.load_forecast(api_key, latitude, longitude).currently()
-	summary = forecast.summary
-	temperature = forecast.temperature
-	return "{} and {}° at {}".format(summary, temperature, address)
+#def get_weather(address):
+#	api_key = os.environ['FORECASTIO_API_KEY']
+#	geolocator = Nominatim()
+#	location = geolocator.geocode(address)
+#	latitude = location.latitude
+#	longitude = location.longitude
+#	forecast = forecastio.load_forecast(api_key, latitude, longitude).currently()
+#	summary = forecast.summary
+#	temperature = forecast.temperature
+#	return "{} and {}° at {}".format(summary, temperature, address)
